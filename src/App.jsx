7411e199e7060ef857b6fea3e35a595800fbebd7
inga-divra <ingady@gmail.com>
2024-07-25 17:27:17 +0300

    Added basic routing to the application using React Router
    - Imported createBrowserRouter and RouterProvider from react-router-dom.
    - Defined routes for the home page ('/') and about page ('/about').
    - Set up the RouterProvider with the defined routes in the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h2>home page</h2>
+  },
+  {
+    path: '/about',
+    element: <div>
+      <h2>about</h2>
+    </div>
+  }
+])
+
 const App = () => {
-  return <h2>mixmaster</h2>;
-};
+  return <RouterProvider router={router} />
+}
 export default App;
