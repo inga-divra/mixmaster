0657328aca508561373b66aec79ae4f01c7d8c05
inga-divra <ingady@gmail.com>
2024-07-26 18:21:27 +0300

    Set up Error component to handle 404 and other errors with styled components and custom messages

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,26 @@
+import StyleWrapper from '../assets/styleWrappers/ErrorPage'
+import { Link, useRouteError } from 'react-router-dom'
+import img from '../assets/not-found.svg'
 
 const Error = () => {
-    return (
-        <h1>Error</h1>
-    )
+    const error = useRouteError()
+
+    if (error.status === 404) {
+        return (<StyleWrapper>
+            <div>
+                <img src={img} alt="404 page" />
+                <h3>Ohh!</h3>
+                <p>We can't seem to find page you are looking for</p>
+                <Link to="/">back home</Link>
+            </div>
+        </StyleWrapper>)
+    }
+
+    return <StyleWrapper>
+        <div>
+            <h3>something went wrong</h3>
+        </div>
+    </StyleWrapper>
 }
 
 export default Error
