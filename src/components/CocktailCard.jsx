836da3ddfd867cf81da30fd079cae6d6f2c8d0f2
inga-divra <ingady@gmail.com>
2024-07-26 20:21:20 +0300

    Set up CocktailCard component to render individual cocktail details and link to the cocktail's detailed page

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom'
+import StyleWrapper from '../assets/styleWrappers/CocktailCard'
 
-const CocktailCard = () => {
-    return (
-        <h2>CocktailCard</h2>
-    )
+const CocktailCard = ({ id, image, name, info, glass }) => {
+    return <StyleWrapper>
+        <div className="img-container">
+            <img className="img" src={image} alt="img" />
+        </div>
+        <div className="footer">
+            <h4>{name}</h4>
+            <h5>{glass}</h5>
+            <p>{info}</p>
+            <Link
+                className="btn"
+                to={`/cocktail/${id}`}>details</Link>
+        </div>
+    </StyleWrapper>
 }
 
 export default CocktailCard
