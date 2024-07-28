b1274eadae140a2f3a445a94244c61b98e1b2ec2
inga-divra <ingady@gmail.com>
2024-07-28 16:05:00 +0300

    Set up form structure and imported Form component from react-router-dom
    - Imported the Form component from react-router-dom
    - Set up a basic form structure in the Newsletter component
    - Included fields for name, last name, and email with default values
    - Added a submit button to the form

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { Form } from 'react-router-dom'
+
 
 const Newsletter = () => {
-    return (
-        <h1>Newsletter</h1>
-    )
+    return <Form className="form">
+        <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
+            our newsletter
+        </h4>
+        {/* name */}
+        <div className="form-row">
+            <label className="form-label" htmlFor="name">
+                name
+            </label>
+            <input
+                className="form-input"
+                type="text"
+                name="name"
+                id="name"
+                defaultValue="Peter" />
+        </div>
+        {/* lastName */}
+        <div className="form-row">
+            <label className="form-label" htmlFor="lastName">
+                last name
+            </label>
+            <input
+                className="form-input"
+                type="text"
+                name="lastName"
+                id="lastName"
+                defaultValue="Wolt" />
+        </div>
+        {/* email */}
+        <div className="form-row">
+            <label className="form-label" htmlFor="email">
+                last name
+            </label>
+            <input
+                className="form-input"
+                type="text"
+                name="email"
+                id="email"
+                defaultValue="test@test.com" />
+        </div>
+        <button
+            className="btn btn-block"
+            type="submit"
+            style={{ marginTop: '0.5rem' }}
+        >
+            submit
+        </button>
+    </Form>
 }
 
 export default Newsletter
