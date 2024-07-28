875ca8533766c47ee754f7a0e98d733ca01769b0
inga-divra <ingady@gmail.com>
2024-07-28 19:25:35 +0300

    Added submit button state handling in Newsletter form
    - Imported useNavigation from react-router-dom
    - Used useNavigation to track form submission state
    - Disabled submit button while the form is submitting
    - Changed button text to 'submitting' during form submission
    - Ensured submit button reverts to 'submit' after submission is complete

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from 'react-router-dom';
+import { Form, redirect, useNavigation } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
@@ -23,8 +23,10 @@ export const action = async ({ request }) => {
     }
 }
 
-
 const Newsletter = () => {
+    const navigation = useNavigation()
+    const isSubmitting = navigation.state === 'submitting'
+
     return <Form className="form" method='POST'>
         <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
             our newsletter
@@ -69,8 +71,9 @@ const Newsletter = () => {
             className="btn btn-block"
             type="submit"
             style={{ marginTop: '0.5rem' }}
+            disabled={isSubmitting}
         >
-            submit
+            {isSubmitting ? 'submitting' : 'submit'}
         </button>
     </Form>
 }
